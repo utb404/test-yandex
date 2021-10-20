891d4150818658b4041e3ffec39343e992a7bc08
Artem Mikhaylov <me@utb404.ru>
2021-10-20 20:45:25 +0500

    Реализовал переход на страницу поиска

--- test_main_page.py
+++ test_main_page.py
@@ -3,7 +3,10 @@ from pages.main_page import MainPage
 url = 'https://yandex.ru'
 word_for_search = 'тензор'
 
-def test_should_see_search_string(browser):
+def test_search_string(browser):
 	main_page = MainPage(browser, url)
 	main_page.open()
 	main_page.should_see_search_string()
+	main_page.insert_word_to_search_string(word_for_search)
+	main_page.should_see_suggest()
+	main_page.press_enter()
