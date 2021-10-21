ba3c5b294a750768fdda34eebb2717efd4438fb1
Artem Mikhaylov <me@utb404.ru>
2021-10-21 09:47:52 +0500

    Добавил возможность выбора firefox

--- conftest.py
+++ conftest.py
@@ -1,9 +1,17 @@
 import pytest
 from selenium import webdriver
 
+def pytest_addoption(parser):
+	parser.addoption('--browser_name', action='store',
+			 default='chrome', help='Выбери браузер: chrome или firefox')
 
 @pytest.fixture
-def browser():
-	driver = webdriver.Chrome()
+def browser(request):
+	browser_name = request.config.getoption('browser_name')
+	browser = None
+	if browser_name == 'chrome':
+		browser = webdriver.Chrome()
+	else:
+		browser = webdriver.Firefox()
 	yield driver
 	driver.quit()
